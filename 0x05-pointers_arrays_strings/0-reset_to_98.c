fecd5b815a2dda618e131bef14b33b838c93e480
Cool-lins <collinseonyekwere@gmail.com>
2022-09-19 06:13:25 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of n points to 98
+ * @n: Pointer given by the name function
+ * Descrition: updates the value it prints
+ *
+ */
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
