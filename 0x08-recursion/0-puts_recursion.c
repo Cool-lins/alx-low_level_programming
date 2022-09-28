7632d6fd858d81198f1f0f3b06f849d671cb8997
Cool-lins <collinseonyekwere@gmail.com>
2022-09-28 14:51:02 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a new line
- * @s: string given by main
- * Return: none or 0
+ * _puts_recursion - print a spring
+ * @s: Pointer
+ * Return: Always 0.
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == 0)/*using pointer to get s value*/
+	if (s[0] == '\0')
 	{
-		_putchar(10);
+		_putchar('\n');
 		return;
 	}
-	_putchar(*s);
+
+	_putchar(s[0]);
 	_puts_recursion(s + 1);
 }
