ea9d7dbacb77e1ef8550d6340134e1991cff1509
Cool-lins <collinseonyekwere@gmail.com>
2022-10-10 11:44:05 -0700

    1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,20 +1,4 @@
-#include "1-pi.h"
-#include "1-pi.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-    float a;
-    float r;
-
-    r = 98;
-    a = PI * r * r;
-    printf("%.3f\n", a);
-    return (0);
-}
+#ifndef SANBRUCE
+#define SANBRUCE
+#define PI 3.14159265359
+#endif
